fef1ea29042a11e21650aecfe7a634b118a63faf
Harsh Bali <85778411+Harsh72019@users.noreply.github.com>
2025-04-29 21:42:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,16 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import fileUpload from 'express-fileupload';
 import dotenv from 'dotenv';
-
 import connectDB from './src/config/db.config.js';
 import redisClient from './src/config/redis.config.js';
-
 import mainRouter from "./src/routes/index.js";
 import errorHandler from './src/middlewares/errorHandler.js';
 import { swaggerUi, swaggerSpec } from './src/config/swagger.config.js';
 
-
 dotenv.config();
+
 const app = express();
 
 connectDB();
@@ -27,14 +25,15 @@ app.use(fileUpload({
     tempFileDir: '/tmp/',
 }));
 
-app.use("/" , (req , res) => {
-    res.send(`Task for Idea Usher`)
-});
 // Routes
 app.use('/api/v1', mainRouter);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+// Home route - this should come AFTER other specific routes
+app.use("/", (req, res) => {
+    res.send(`Task for Idea Usher`)
+});
 
 // Error handler
 app.use(errorHandler);
